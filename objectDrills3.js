f197a56e8ad5715704069c4e03c55f3191eef3dd
Jvymvn <Jordon.carter@yahoo.com>
2019-06-05 18:43:18 -0400

    LOTR

--- objectDrills3.js
+++ objectDrills3.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 'use strict';
 
+/*
+
 const loaf = {
   flour: 300,
   water: 210,
@@ -96,3 +98,66 @@ console.log(decode(word));
 
 /////////////////////////
 
+*/
+
+
+function createCharacter(Name, Nickname, Race, Origin, Attack, Defense){
+return {
+  name: Name,
+  nickname: Nickname, 
+  race: Race, 
+  origin: Origin, 
+  attack: Attack, 
+  defense: Defense,
+    describe: function() {
+  console.log(`${this.name} is a ${this.race} from ${this.origin}.`);},
+
+    evaluateFight: function(char1, char2) {
+  if(attack > defense){
+    // let x = 0;
+    // let y = this.defense - this.attack;
+    return `Your opponent takes ${this.x} damage and you receive ${this.y} damage`;
+  } else if (defense > attack){
+    return `Your opponent takes 0 damage.`;
+  }
+}
+}
+}
+let Gandalf = createCharacter('Gandalf the White','gandalf','Wizard','Middle Earth',10,6);
+let Bilbo = createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1);
+let Frodo = createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2);
+let Aragorn = createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8);
+let Legolas = createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5);
+let Arwen = createCharacter('Arwen Undomiel', "arwen", "Half-Elf", "Rivendell", 50, 75);
+
+
+let characters = [
+  Gandalf,
+  Bilbo,
+  Frodo,
+  Aragorn,
+  Legolas,
+  Arwen
+];
+
+
+console.log(characters);
+
+let found = characters.find(function(element){
+  if(element.nickname === 'aragorn'){
+    return element.describe();
+  }
+})
+
+let filtered = characters.filter(word => word.race === 'Hobbit');
+let filteredAtk = characters.filter(word => word.attack > 5);
+// let filtered = characters.filter(function(word){
+//   if (word.race === 'Hobbit'){
+//     return word.name;
+//   }
+// })
+
+console.log(found);
+console.log(filtered);
+console.log(filteredAtk);
+//evaluateFight(Bilbo, Frodo);
