687525b61c1499fbee0bc88e74f112c2d99414c2
Corey Moore <monorthwest@gmail.com>
2019-06-05 13:00:17 -0700

    added decoder function

--- objectDrills3.js
+++ objectDrills3.js
@@ -44,20 +44,30 @@ console.log(object.meals[3]);
 
 let people = [
   {Name: 'Bob',
-  job: 'Plumber'},
+    Job: 'Plumber'},
   {Name: 'Fred',
-  job: 'Architect'},
+    Job: 'Architect',
+    Boss: 'Bob'},
   {Name: 'Jenny',
-  job: 'Scientist'},
+    Job: 'Scientist',
+    Boss: 'Bob'},
   {Name: 'Ted',
-  job: 'Host'},
+    Job: 'Host',
+    Boss: 'Bob'},
   {Name: 'Spongebob',
-  job: 'Chef'}
+    Job: 'Chef',
+    Boss: 'Bob'}
 ];
 
 function occupation(obj){
   for(let key in obj){
     console.log(obj[key]);
+   
+    if(obj[key].Boss === undefined){
+      console.log(`${obj[key].Job} ${obj[key].Name} doesn't report to anybody.`);
+    } else {
+      console.log(`${obj[key].Job} ${obj[key].Name} reports to ${obj[key].Boss}.`);
+    } 
   }
 }
 
@@ -65,3 +75,31 @@ occupation(people);
 
 /////////////////////////
 
+let sentence = 'craft block argon meter bells brown croon droop';
+
+let code = '';
+
+let word = sentence.split(' ');
+
+const deCoder = {
+  'a': 2,
+  'b': 3,
+  'c': 4,
+  'd': 5,
+};
+
+function decode(word){
+
+  for(let i = 0; i < word.length; i++){
+    if(word[i][0] === deCoder['a']) {code = Object.values('a');}
+    
+    if(word[i][0] === deCoder['b']) {}
+
+    if(word[i][0] === deCoder['c']) {}
+
+    if(word[i][0] === deCoder['d']) {
+
+   } else {code = code + ' ';
+  }
+}
+}
