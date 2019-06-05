d0f7503f19d34c68f8d6d2be679d5dc45fcbcf2e
Corey Moore <monorthwest@gmail.com>
2019-06-05 08:52:33 -0700

    added object creator drill

--- objectDrills1.js
+++ objectDrills1.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function createMyObject(){
+  const newObj = {
+    foo: 'bar',
+    answerToTheUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello() {
+      console.log('hello');
+    }
+  };
+}
+// console.log(createMyObject);
+
+/////////////////////////
+
