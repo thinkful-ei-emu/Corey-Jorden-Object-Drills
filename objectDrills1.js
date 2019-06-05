e105c5a76f29fbff1d73bfd28d8e66e28ed5ed47
Jvymvn <Jordon.carter@yahoo.com>
2019-06-05 12:06:38 -0400

    self reference drill

--- objectDrills1.js
+++ objectDrills1.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function createMyObject(){
   const newObj = {
     foo: 'bar',
@@ -12,5 +12,27 @@ function createMyObject(){
 }
 // console.log(createMyObject);
 
+*/
+
 /////////////////////////
 
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+
+/////////////////////////
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    }
+  };
+  return person;
+}
